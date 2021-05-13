37e435e695f7a952daa7d8098f13d37559a58994
Salvoray <zzoomikk@gmail.com>
2021-05-13 16:38:05 +0300

    added markup script with handlebars

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './styles.css';
+import dishesTpl from './templates/menu-list.hbs';
+import menu from './data/menu';
+
+console.log(menu);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const menuContainer = document.querySelector('.js-menu');
+const toggle = document.querySelector('#theme-switch-toggle');
+
+const dishesMarkup = dishesTpl(menu);
+
+menuContainer.insertAdjacentHTML('afterbegin', dishesMarkup);
