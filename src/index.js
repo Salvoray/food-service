012b5711080ae7af420b149bbd3be69858e32108
Salvoray <zzoomikk@gmail.com>
2021-05-21 12:54:47 +0300

    added logic to change theme on switch toggle

--- src/index.js
+++ src/index.js
@@ -10,6 +10,30 @@ const Theme = {
 const menuContainer = document.querySelector('.js-menu');
 const toggle = document.querySelector('#theme-switch-toggle');
 
+const onChangeToggle = e => {
+  if (e.target.checked) {
+    document.body.classList.remove(Theme.LIGHT);
+    document.body.classList.add(Theme.DARK);
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    document.body.classList.remove(Theme.DARK);
+    document.body.classList.add(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+};
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+  document.body.classList.add(currentTheme);
+
+  if (currentTheme === Theme.DARK) {
+    toggle.checked = true;
+  }
+}
+
+toggle.addEventListener('change', onChangeToggle);
+
 const dishesMarkup = dishesTpl(menu);
 
 menuContainer.insertAdjacentHTML('afterbegin', dishesMarkup);
